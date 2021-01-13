e93021966f758b5360547c4f15ced40b04d71c5f
harshan3r <harshanranges3@gmail.com>
2021-01-13 16:47:43 +0530

    Added Card layout and UI formattings

--- app/src/main/java/com/example/covidsldash/MainActivity.java
+++ app/src/main/java/com/example/covidsldash/MainActivity.java
@@ -7,19 +7,40 @@ import androidx.fragment.app.FragmentManager;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.ismaeldivita.chipnavigation.ChipNavigationBar;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class MainActivity extends AppCompatActivity {
     private static final String TAG = MainActivity.class.getSimpleName();
     ChipNavigationBar bottomNav;
     FragmentManager fragmentManager;
+
+    TextView mTextViewResult;
+    private RequestQueue mRequestQueue;
+    private StringRequest mStringRequest;
+    private String url = "http://www.mocky.io/v2/597c41390f0000d002f4dbd1";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         bottomNav = findViewById(R.id.bottom_nav);
+
+        Button buttonParse = findViewById(R.id.refresh);
 
         if(savedInstanceState==null){
             bottomNav.setItemSelected(R.id.home,true);
@@ -56,5 +77,9 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
+
+
     }
+
+
 }
